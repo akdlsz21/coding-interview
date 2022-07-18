2946468fd3c6bbad2386a8c967216f750be6282d
akdlsz21 <akdlsz21@gmail.com>
2022-07-18 21:22:45 +0900

    use two pointers, slow and fast

--- SLL/reverse linked list.js
+++ SLL/reverse linked list.js
@@ -1,14 +1,18 @@
 var reverseList = function (head) {
-	let curr = head;
-	let temp = curr;
-	let ne = head.next;
-	curr.next = null;
-	while (ne) {
-		let third = ne.next;
-		ne.next = curr;
-		curr = ne;
-		ne = third;
-	}
+	const temp = new ListNode(-1);
+	if (!head) return null;
 
-	return temp;
+	let prev = null;
+	let curr = head;
+	let nex = head.next;
+	temp.next = curr;
+	while (nex) {
+		curr.next = prev;
+		prev = curr;
+		curr = nex;
+		nex = nex.next;
+	}
+	curr.next = prev;
+
+	return curr;
 };
