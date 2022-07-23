6c49e3cd416f7788bbd65ebda5e63d0e71e26070
akdlsz21 <akdlsz21@gmail.com>
2022-07-24 00:42:45 +0900

    aware of infinite loop, aware of what needs to be retuned in dfs

--- graph/number_of_islands.js
+++ graph/number_of_islands.js
@@ -1,11 +1,8 @@
 var numIslands = function (grid) {
 	let count = 0;
 	for (let i = 0; i < grid.length; i++) {
-		for (let j = 0; j < grid[i].length; j++) {
-			let val = grid[i][j];
-			if (val === '1') {
-				count += dfs(grid, i, j);
-			}
+		for (let j = 0; j < grid[0].length; j++) {
+			count += dfs(grid, i, j);
 		}
 	}
 
@@ -13,27 +10,23 @@ var numIslands = function (grid) {
 };
 
 function dfs(grid, i, j) {
-	if (
-		i < 0 ||
-		i >= grid.length ||
-		j < 0 ||
-		j >= grid[i].length ||
-		grid[i][j] === '0'
-	)
-		return 0;
-	grid[i][j] = '0';
-	dfs(grid, i - 1, j);
-	dfs(grid, i + 1, j);
-	dfs(grid, i, j - 1);
-	dfs(grid, i, j + 1);
+	let isValidCondition =
+		i >= 0 && i < grid.length && j >= 0 && j < grid[0].length;
+
+	if (isValidCondition && grid[i][j] === '1') {
+		grid[i][j] = '0';
+		dfs(grid, i + 1, j);
+		dfs(grid, i - 1, j);
+		dfs(grid, i, j + 1);
+		dfs(grid, i, j - 1);
+	} else return 0;
 
 	return 1;
 }
 
-const grid = [
+numIslands([
+	['1', '1', '1', '1', '0'],
+	['1', '1', '0', '1', '0'],
 	['1', '1', '0', '0', '0'],
-	['1', '1', '0', '0', '0'],
-	['0', '0', '1', '0', '0'],
-	['0', '0', '0', '1', '1'],
-];
-numIslands(grid); //?
+	['0', '0', '0', '0', '0'],
+]); //?
