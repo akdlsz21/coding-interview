a695bb239807813f238a9614f8721bf84124ff64
akdlsz21 <akdlsz21@gmail.com>
2022-07-24 00:52:22 +0900

    dp, using hashmap, focus on memoization

---
+++ DP/Fibonacci Number.js
@@ -0,0 +1,13 @@
+var fib = function (n) {
+	const map = {};
+
+	function helper(n) {
+		if (n <= 0) return 0;
+		if (n <= 2) return 1;
+		if (map[n]) return map[n];
+
+		map[n] = helper(n - 1) + helper(n - 2);
+		return map[n];
+	}
+	return helper(n - 1) + helper(n - 2);
+};
